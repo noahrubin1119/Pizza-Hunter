42f82ce3d4d483b2eec8700035de649cd346a99c
Noah Rubin <noahgidon@gmail.com>
2018-07-11 13:51:22 -0500

    zoom to location

--- Pizza Hunter/ViewController.swift
+++ Pizza Hunter/ViewController.swift
@@ -7,11 +7,20 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    let locationManager = CLLocationManager()
+    var region = MKCoordinateRegion()
     override func viewDidLoad() {
         super.viewDidLoad()
+        locationManager.requestWhenInUseAuthorization()
+        mapView.showsUserLocation = true
+        locationManager.delegate = self
+        locationManager.startUpdatingLocation()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,6 +28,16 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.first!
+        let center = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        let span = MKCoordinateSpanMake(0.025, 0.025)
+        region = MKCoordinateRegionMake(center, span)
+        mapView.setRegion(region, animated: true)
+    }
+    
+    
 
 
 }
